75f877940442735971667ab7a57a92cbca214280
Eval427 <zcommon03@gmail.com>
2025-07-31 15:53:43 -0700

    Moreeee

--- dist/server.js
+++ dist/server.js
@@ -15,10 +15,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
+app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
 let imgUrl;
 // Discord -> Server
 app.post('/inbound', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
